07f6bafa2ab10e82ed2c0f48cc1f1ea5f07bf809
MinhNguyen179 <mulac179@gmail.com>
2023-08-07 20:47:53 +0700

    Fix css and custom login

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
-<?PHP
-$user_name = "root";
-$password = "Muaxuan179";
-$database = "php-todo";
-$host_name = "localhost";
-// Create connection
-$conn = new mysqli($host_name, $user_name, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Query to get all table names
-$query = "SHOW TABLES";
-$result = $conn->query($query);
-
-// Fetch table names
-//if ($result->num_rows > 0) {
-//    while($row = $result->fetch_row()) {
-//        echo "Table: " . $row[0] . "<br>";
-//    }
-//} else {
-//    echo "0 tables found";
-//}
-
-//// Close connection
-//$conn->close();
-?>
 <!doctype html>
 <html lang="en">
 <head>
