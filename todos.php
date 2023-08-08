71ef7ca605ac27ee63356c05375471a54de80e8d
MinhNguyen179 <mulac179@gmail.com>
2023-08-08 10:34:19 +0700

    Fix the To do page

--- todos.php
+++ todos.php
@@ -25,66 +25,78 @@ $todolist = $todolist->fetchAll(PDO::FETCH_ASSOC);
 $total = $db -> query("SELECT FOUND_ROWS() as total")->fetch()['total'];
 $pages = ceil($total/$perPage);
 
-?><div class="container">
-    <form>
-        <h2 class = "text-center my-5">THIS IS YOUR TODO LIST!!</h2>
-        <div class = "row justify-content-center">
-            <div class = "col-md-10">
-                <div class ="card card-default">
-                    <div class = "card-header">Todo list</div>
-                    <div class = "card-body">
-                        <ul class="list-group">
-                            <?php
-                            foreach ($todolist as $todo) {
-                                echo '<li class="list-group-item flex">';
-                                echo $todo['title'];
-
-                                if (!$todo['completed']) {
-                                    echo '<a href="/todos.php/' . $todo['id'] . '/complete.php" style="color:black" class="btn btn-warning btn float-right">Complete</a>';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SIGN UP</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<form action="todos.php" method="post">
+    <div class="container">
+        <form>
+            <h2 class = "text-center my-5">THIS IS YOUR TODO LIST!!</h2>
+            <div class = "row justify-content-center">
+                <div class = "col-md-10">
+                    <div class ="card card-default">
+                        <div class = "card-header">Todo list</div>
+                        <div class = "card-body">
+                            <ul class="list-group">
+                                <?php
+                                foreach ($todolist as $todo) {
+                                    echo '<li class="list-group-item d-flex justify-content-between">';
+                                    echo $todo['title'];
+                                    echo '<div class="action-links">';
+                                    if (!$todo['completed']) {
+                                        echo '<a href="/todos.php/' . $todo['id'] . '/complete.php" class="complete-btn btn btn-warning btn float-right">Complete</a>';
+                                    }
+                                    echo '<a href="/todos.php/' . $todo['id'] . '/complete.php" class="view-btn btn-primary btn float-right mr-2">View</a>';
+                                    echo '</div>';
+                                    echo '</li>';
                                 }
-
-                                echo '<a href="/todos.php/' . $todo['id'] . '/complete.php" class="btn btn-primary btn float-right mr-2">View</a>';
-                                echo '</li>';
-                            }
-                            ?>
-                        </ul>
+                                ?>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </form>
-    <div class="row justify-content-center my-3">
-        <div class="pagination">
-            <?php
-            echo '<style>.page-number { margin-right: 10px; }</style>';
+        </form>
+        <div class="row justify-content-center my-3">
+            <div class="pagination">
+                <?php
+                echo '<style>.page-number { margin-right: 10px; }</style>';
 
-            $maxVisiblePages = 5;
-            $halfMaxVisiblePages = floor($maxVisiblePages / 2);
-            $startPage = max(1, $currentPage - $halfMaxVisiblePages);
-            $endPage = min($startPage + $maxVisiblePages - 1, $pages);
+                $maxVisiblePages = 5;
+                $halfMaxVisiblePages = floor($maxVisiblePages / 2);
+                $startPage = max(1, $currentPage - $halfMaxVisiblePages);
+                $endPage = min($startPage + $maxVisiblePages - 1, $pages);
 
-            if ($startPage > 1) {
-                echo '<span class="page-number"><a href="/todos.php/?page=1&per-page=' . $perPage . '"> << </a></span>';
-                echo '<span class="page-number"><a href="/todos.php/?page=' . ($currentPage - 1) . '&per-page=' . $perPage . '"> ...</a></span>';
-            }
+                if ($startPage > 1) {
+                    echo '<span class="page-number"><a href="/todos.php/?page=1&per-page=' . $perPage . '"> << </a></span>';
+                    echo '<span class="page-number"><a href="/todos.php/?page=' . ($currentPage - 1) . '&per-page=' . $perPage . '"> ...</a></span>';
+                }
 
-            for ($x = $startPage; $x <= $endPage; $x++) {
-                echo '<span class="page-number"><a href="/todos.php/?page=' . $x . '&per-page=' . $perPage . '">' .$x. '</a></span>';
-            }
+                for ($x = $startPage; $x <= $endPage; $x++) {
+                    echo '<span class="page-number"><a href="/todos.php/?page=' . $x . '&per-page=' . $perPage . '">' .$x. '</a></span>';
+                }
 
-            if ($endPage < $pages) {
-                echo '<span class="page-number"><a href="/todos.php/?page=' . ($currentPage + 1) . '&per-page=' . $perPage . '">...</a></span>';
-                echo '<span class="page-number"><a href="/todos.php/?page=' . $pages . '&per-page=' . $perPage . '"> >> </a></span>';
-            }
-            ?>
+                if ($endPage < $pages) {
+                    echo '<span class="page-number"><a href="/todos.php/?page=' . ($currentPage + 1) . '&per-page=' . $perPage . '">...</a></span>';
+                    echo '<span class="page-number"><a href="/todos.php/?page=' . $pages . '&per-page=' . $perPage . '"> >> </a></span>';
+                }
+                ?>
 
+            </div>
         </div>
     </div>
-</div>
+</form>
+</body>
+</html>
 <?php
     }else{
         header("Location: index.php");
         exit();
     }
 ?>
-
