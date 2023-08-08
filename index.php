da8edd4e0630aa0b2a2198f024aa1a0f2249c6fc
MinhNguyen179 <mulac179@gmail.com>
2023-08-08 09:08:37 +0700

    Finish custom register page

--- index.php
+++ index.php
@@ -10,16 +10,20 @@
 </head>
 <body>
     <form action="login.php" method = "POST">
+        <h1><strong>Welcome to Todo List App</strong></h1>
+        <p  style="text-align: center; margin-top: 30px"><i>"Manage your tasks efficiently and never miss a deadline."</i></p>
+        <div>
+            <?php if (isset($_GET['error'])){ ?>
+                <p class = "error"><?php echo $_GET['error'];?></p>
+            <?php }?>
+        </div>
         <h2>Login Page</h2>
-        <?php if (isset($_GET['error'])){ ?>
-            <p class = "error"><?php echo $_GET['error'];?></p>
-        <?php }?>
         <label>User Name</label>
         <input type = "text" name = "uname" placeholder="Username...">
         <label>Password</label>
         <input type = "password" name = "password" placeholder="Password...">
-        <button type="submit">Submit</button>
-        <a href="signup.php" class="ca">Create an account</a>
+        <button type="submit" class = "submit-button" >Submit</button>
+        <a href="signup.php" class="submit-button">Create an account</a>
     </form>
 </body>
 </html>
